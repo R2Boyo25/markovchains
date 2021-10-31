0f8567ec697a86136ee95820ed4a6e342dc3baf9
R2boyo25 <r2boyo25@gmail.com>
2021-10-31 14:06:02 -0500

    Usage

--- mark.cpp
+++ mark.cpp
@@ -9,22 +9,25 @@ using json = nlohmann::json;
 
 int main(int argc, char* argv[]) {
     MarkovGenerator markov;
-    if (string(argv[2]) == "to") {
-        markov.fromFile(argv[1]);
+    if (argv > 1) {
+       if (string(argv[2]) == "to") {
+            markov.fromFile(argv[1]);
 
-        ofstream o(argv[3]);
-        o << setw(4) << markov.toJSON() << std::endl;
+            ofstream o(argv[3]);
+            o << setw(4) << markov.toJSON() << std::endl;
+        }
+        else if (string(argv[1]) == "from") {
+            int len = atoi(argv[3]);
+            ifstream i(argv[2]);
+            stringstream s;
+            s << i.rdbuf();
+
+            markov.fromJSON(s.str());
+            cout << markov.generate(len);
+        } 
+    } else {
+        cout << "Usage:\n\tmark textfile to cachefile\n\tmark from cachefile lentogenerate" << endl;
     }
-    else if (string(argv[1]) == "from") {
-        int len = atoi(argv[3]);
-        ifstream i(argv[2]);
-        stringstream s;
-        s << i.rdbuf();
-
-        markov.fromJSON(s.str());
-        cout << markov.generate(len);
-    } 
-
     return 0;
 
 }
